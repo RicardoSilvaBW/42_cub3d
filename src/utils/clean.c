0147c0872fbbf330ad5ea57cb2d1415cc18f67b1
feazeved <felpssuassuna@gmail.com>
2026-06-10 15:30:09 +0100

    fixed norm in utils folder

--- src/utils/clean.c
+++ src/utils/clean.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   clean.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rjorge-p < rjorge-p@student.42porto.com    +#+  +:+       +#+        */
+/*   By: feazeved <feazeved@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 11:12:51 by feazeved          #+#    #+#             */
 /*   Updated: 2026/06/09 15:47:03 by rjorge-p         ###   ########.fr       */
@@ -15,47 +15,47 @@
 
 void	free_split(char **split)
 {
-    int	i;
+	int	i;
 
-    if (!split)
-        return ;
-    i = 0;
-    while (split[i])
-    {
-        free(split[i]);
-        i++;
-    }
-    free(split);
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
 }
 
-void  free_parse(t_data *data)
+void	free_parse(t_data *data)
 {
-    if (data->map.file)
-        free_split(data->map.file);
-    if (data->textures.north_path)
-        free(data->textures.north_path);
-    if (data->textures.south_path)
-        free(data->textures.south_path);
-    if (data->textures.west_path)
-        free(data->textures.west_path);
-    if (data->textures.east_path)
-        free(data->textures.east_path);
+	if (data->map.file)
+		free_split(data->map.file);
+	if (data->textures.north_path)
+		free(data->textures.north_path);
+	if (data->textures.south_path)
+		free(data->textures.south_path);
+	if (data->textures.west_path)
+		free(data->textures.west_path);
+	if (data->textures.east_path)
+		free(data->textures.east_path);
 }
 
-void    clean(t_data *data)
+void	clean(t_data *data)
 {
 	free_parse(data);
 	if (data->mlx && data->win)
 		mlx_destroy_window(data->mlx, data->win);
 	if (data->mlx && data->textures.north.img)
-        mlx_destroy_image(data->mlx, data->textures.north.img);
-    if (data->mlx && data->textures.south.img)
-        mlx_destroy_image(data->mlx, data->textures.south.img);
-    if (data->mlx && data->textures.west.img)
-        mlx_destroy_image(data->mlx, data->textures.west.img);
-    if (data->mlx && data->textures.east.img)
-        mlx_destroy_image(data->mlx, data->textures.east.img);
-    if (data->mlx && data->frame.img)
+		mlx_destroy_image(data->mlx, data->textures.north.img);
+	if (data->mlx && data->textures.south.img)
+		mlx_destroy_image(data->mlx, data->textures.south.img);
+	if (data->mlx && data->textures.west.img)
+		mlx_destroy_image(data->mlx, data->textures.west.img);
+	if (data->mlx && data->textures.east.img)
+		mlx_destroy_image(data->mlx, data->textures.east.img);
+	if (data->mlx && data->frame.img)
 		mlx_destroy_image(data->mlx, data->frame.img);
 	if (data->mlx)
 		mlx_destroy_display(data->mlx);
